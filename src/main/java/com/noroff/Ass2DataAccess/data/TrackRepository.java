68761047d4d9cf7c1094713d43533ab6a254b11d
1l0v3m0un741n5 <>
2022-02-23 18:29:20 +0200

    fixes to search function

--- src/main/java/com/noroff/Ass2DataAccess/data/TrackRepository.java
+++ src/main/java/com/noroff/Ass2DataAccess/data/TrackRepository.java
@@ -47,14 +47,12 @@ public class TrackRepository implements com.noroff.Ass2DataAccess.data.interface
         try (ConnectionManager mng = ConnectionManager.getInstance()){
             Connection conn = mng.getConnection();
             PreparedStatement preparedStatement = conn.prepareStatement(
-                    "SELECT Track.TrackId AS TrackId, " +
-                            "Track.Name AS TrackName, " +
+                    "SELECT Track.TrackId AS TrackId, Track.Name AS TrackName, " +
                             "Album.Title AS Title, " +
                             "Genre.Name AS GenreName " +
-                            "FROM Track " +
-                            "INNER JOIN Album ON Track.AlbumId = Album.AlbumId" +
+                            "FROM Track INNER JOIN Album ON Track.AlbumId = Album.AlbumId " +
                             "INNER JOIN Genre ON Track.GenreId = Genre.GenreId " +
-                            "WHERE TrackName LIKE ?" +
+                            "WHERE TrackName LIKE ? " +
                             "GROUP BY Track.TrackId, Track.Name, Track.Composer " +
                             "ORDER BY TrackId DESC");
 
@@ -65,7 +63,7 @@ public class TrackRepository implements com.noroff.Ass2DataAccess.data.interface
             while(resultSet.next()) {
                 Track track = new Track();
                 track.setTrackId(resultSet.getInt("TrackId"));
-                track.setName(resultSet.getString("Name"));
+                track.setName(resultSet.getString("TrackName"));
 
                 Album album = new Album();
                 album.setTitle(resultSet.getString("Title"));
