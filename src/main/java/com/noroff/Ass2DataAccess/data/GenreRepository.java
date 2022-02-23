07f3d903625b56761630b1d36998dd6395b8f3e2
1l0v3m0un741n5 <>
2022-02-23 16:55:16 +0200

    added functino for getting random genre

--- src/main/java/com/noroff/Ass2DataAccess/data/GenreRepository.java
+++ src/main/java/com/noroff/Ass2DataAccess/data/GenreRepository.java
@@ -2,10 +2,39 @@ package com.noroff.Ass2DataAccess.data;
 
 import com.noroff.Ass2DataAccess.models.Genre;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class GenreRepository implements com.noroff.Ass2DataAccess.data.interfaces.GenreRepository{
     public List<Genre> getRandom(int amount) {
-        return null;
+        List<Genre>list = new ArrayList<>();
+
+        try (ConnectionManager mng = ConnectionManager.getInstance()){
+            Connection conn = mng.getConnection();
+            PreparedStatement preparedStatement = conn.prepareStatement(
+                    "SELECT GenreId, Name FROM Genre ORDER BY random() LIMIT ?");
+
+            preparedStatement.setString(1, toString().valueOf(amount));
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while(resultSet.next()) {
+                Genre genre = new Genre();
+                genre.setGenreId(resultSet.getInt("GenreId"));
+                genre.setName(resultSet.getString("Name"));
+
+                list.add(genre);
+            }
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+
+        return list;
     }
 }
